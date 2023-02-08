0bececea972e09a123ad36ce62406643d46ed589
reflexguru <trianglestudioad@gmail.com>
2023-02-08 23:57:29 +0300

    finish the scaling code

--- viewport.gd
+++ viewport.gd
@@ -2,8 +2,6 @@ extends SubViewportContainer
 
 @onready var vp = $SubViewport
 
-var current_pixel_scale = 1.0
-
 func _ready() -> void:
 	Thunder.stage_changed.connect(func():
 		get_tree().root.remove_child.call_deferred(Thunder._current_stage)
@@ -18,4 +16,6 @@ func _update_view():
 	if !vp: return
 	
 	var window_size = DisplayServer.window_get_size()
-	scale = Vector2(window_size) / Vector2(vp.size)
+	scale.x = float(window_size.y) / float(vp.size.y)
+	scale.y = float(window_size.y) / float(vp.size.y)
+	vp.size.x = 480 * (float(window_size.x) / float(window_size.y))
