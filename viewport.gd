f33e9183df1ddfa61c342accf9042beac3f11df0
jue131 <83157668+jue131@users.noreply.github.com>
2023-02-09 16:28:04 +0900

    Consistent scaling on native res

--- viewport.gd
+++ viewport.gd
@@ -4,9 +4,10 @@ extends SubViewportContainer
 
 func _ready() -> void:
 	Thunder.stage_changed.connect(func():
+		if !Thunder._current_stage.is_inside_tree(): return
 		get_tree().root.remove_child.call_deferred(Thunder._current_stage)
 		vp.add_child.call_deferred(Thunder._current_stage)
-		get_tree().set_deferred("current_scene", Thunder._current_stage)
+		_update_view()
 	)
 
 func _on_window_resized():
@@ -16,6 +17,8 @@ func _update_view():
 	if !vp: return
 	
 	var window_size = DisplayServer.window_get_size()
+	material.set_shader_parameter(&"enable", scale != Vector2.ONE)
 	scale.x = float(window_size.y) / float(vp.size.y)
 	scale.y = float(window_size.y) / float(vp.size.y)
 	vp.size.x = 480 * (float(window_size.x) / float(window_size.y))
+	
