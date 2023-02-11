62e02e96711a374f7bb48b08724463bb91c3ad87
jue131 <83157668+jue131@users.noreply.github.com>
2023-02-11 16:42:12 +0900

    fix crash

--- viewport.gd
+++ viewport.gd
@@ -4,16 +4,23 @@ extends SubViewportContainer
 
 func _ready() -> void:
 	resized.connect(_on_window_resized)
+	#get_tree().root.audio_listener_enable_2d = false
 	
-	Thunder.stage_changed.connect(func():
-		if !Thunder._current_stage.is_inside_tree(): return
-		get_tree().root.remove_child.call_deferred(Thunder._current_stage)
-		vp.add_child.call_deferred(Thunder._current_stage)
-		_update_view()
-	)
+	#Scenes.install_scene()
+	#
+	add_viewport.call_deferred()
+	Scenes.scene_changed_notification.connect(add_viewport)
 	
-	Audio._calculate_player_position = func(ref: Node2D) -> Vector2:
-		return ref.global_position - vp.get_camera_2d().global_position + Vector2(vp.size / 2)
+	#Audio._calculate_player_position = func(ref: Node2D) -> Vector2:
+	#	return ref.global_position - vp.get_camera_2d().global_position + Vector2(vp.size / 2)
+
+func add_viewport():
+	if !Scenes.current_scene: return
+	if !Scenes.current_scene.is_inside_tree(): return
+	get_tree().root.remove_child.call_deferred(Scenes.current_scene)
+	vp.add_child.call_deferred(Scenes.current_scene)
+	Scenes.current_root = vp
+	_update_view()
 
 func _on_window_resized():
 	_update_view()
